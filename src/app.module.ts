c9364cd38d95d02981590247a1d300d2c45223b9
Timo <ttimoww@gmail.com>
2022-05-19 14:46:32 +0200

    Added empty Checkout module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,7 @@ import { CheckoutModule } from './checkout/checkout.module';
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+            url: process.env.DATABASE_URL,
             synchronize: process.env.NODE_ENV === 'development',
             ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
             logging: false,
