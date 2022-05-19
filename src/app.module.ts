fedf4e644d4b0a7ce876fd39ff7103c099bdd2c0
Timo <ttimoww@gmail.com>
2022-05-19 20:14:29 +0200

    Weet ik veel

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,11 @@ import { CheckoutModule } from './checkout/checkout.module';
     imports: [
         TypeOrmModule.forRoot({
             type: 'postgres',
-            url: process.env.DATABASE_URL,
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             synchronize: process.env.NODE_ENV === 'development',
             ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
             logging: false,
