c4146f27199ab7c8a00b4da1edc4db0d375dcaf4
Timo <ttimoww@gmail.com>
2022-05-19 19:55:42 +0200

    TypeORM update p3

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,5 +5,5 @@ module.exports = {
     'username': 'eqwlablfttxldt',
     'password': 'b4e0cf7a87ed3cbfd111064003210bb45eb96abe6eaf2df8379ce1f38ed705a6',
     'database': 'd2aou930pem1ec',
-    'ssl': false
+    'ssl': true
 };
