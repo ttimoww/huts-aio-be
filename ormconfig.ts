67f125e1da6c087a9edefe32af0199537314df3c
Timo <ttimoww@gmail.com>
2022-05-19 19:48:52 +0200

    TypeORM update p2

---
+++ ormconfig.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    'type': 'postgres',
+    'host': 'ec2-52-18-116-67.eu-west-1.compute.amazonaws.com',
+    'port': 5432,
+    'username': 'eqwlablfttxldt',
+    'password': 'b4e0cf7a87ed3cbfd111064003210bb45eb96abe6eaf2df8379ce1f38ed705a6',
+    'database': 'd2aou930pem1ec',
+    'ssl': false
+};
