04b5bf9712ed6a27679b3837bbb6fec8178e8dc1
Timo <timo@goedgemerkt.nl>
2022-08-02 09:18:42 +0200

    Added update command to Discord bot

--- src/core/entities/update.entity.ts
+++ src/core/entities/update.entity.ts
@@ -1,3 +1,4 @@
+import { UpdateCommandDto } from 'src/discord/dto/update.command.dto';
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
 @Entity()
@@ -13,4 +14,24 @@ export class Update{
 
     @Column()
         url: string;
+    
+    @Column('simple-array', { nullable: true })
+        changelog: string[];
+
+    constructor(dto: UpdateCommandDto){
+        if (dto === undefined) return;
+        this.version = dto.version;
+        this.url = dto.url;
+
+        const changelog = [];
+        if (dto.cl1) changelog.push(dto.cl1);
+        if (dto.cl2) changelog.push(dto.cl2);
+        if (dto.cl3) changelog.push(dto.cl3);
+        if (dto.cl4) changelog.push(dto.cl4);
+        if (dto.cl5) changelog.push(dto.cl5);
+        if (dto.cl6) changelog.push(dto.cl6);
+        if (dto.cl7) changelog.push(dto.cl7);
+        if (dto.cl8) changelog.push(dto.cl8);
+        this.changelog = changelog;
+    }
 }
---
+++ src/discord/dto/update.command.dto.ts
@@ -0,0 +1,40 @@
+import { Param } from '@discord-nestjs/core';
+
+
+export class UpdateCommandDto {
+  @Param({ description: 'Version', required: true })
+      version: string;
+
+  @Param({ description: 'Download URL', required: true })
+      url: string;
+
+  @Param({ description: 'Webhook Notes' })
+      notes: string;
+
+  @Param({ description: 'Webhook Image' })
+      image: string;
+
+    @Param({ description: 'Changelog item one' })
+        cl1?: string;
+
+    @Param({ description: 'Changelog item two' })
+        cl2?: string;
+
+    @Param({ description: 'Changelog item three' })
+        cl3?: string;
+
+    @Param({ description: 'Changelog item four' })
+        cl4?: string;
+
+    @Param({ description: 'Changelog item five' })
+        cl5?: string;
+
+      @Param({ description: 'Changelog item six' })
+          cl6?: string;
+
+      @Param({ description: 'Changelog item seven' })
+          cl7?: string;
+
+      @Param({ description: 'Changelog item eight' })
+          cl8?: string;
+}
