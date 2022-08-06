1f6540e73f0b927f23d79d90aa8417631d6df6a0
Timo <timo@goedgemerkt.nl>
2022-08-06 18:31:57 +0200

    Hotfix: URL in error log optional (this time for real)

--- src/log/dto/module-error-log.dto.ts
+++ src/log/dto/module-error-log.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsString, IsUrl } from 'class-validator';
+import { IsEnum, IsOptional, IsString, IsUrl } from 'class-validator';
 import { Store } from 'src/lib/enums/store.enum';
 
 export default class ModuleErrogLogDto{
@@ -9,6 +9,7 @@ export default class ModuleErrogLogDto{
 
     @ApiPropertyOptional()
     @IsUrl()
+    @IsOptional()
         url?: string;
 
     @ApiProperty()
