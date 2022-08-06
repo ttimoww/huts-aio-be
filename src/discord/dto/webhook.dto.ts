9bb25dbdcc00501b5f2ff7f8e3166c4ea54fc83c
Timo <timo@goedgemerkt.nl>
2022-08-06 22:00:07 +0200

    Hotfix: Webhook Regex to match discordapp.com

--- src/discord/dto/webhook.dto.ts
+++ src/discord/dto/webhook.dto.ts
@@ -3,7 +3,7 @@ import { Matches } from 'class-validator';
 
 
 // eslint-disable-next-line no-useless-escape
-const webhookRegex = new RegExp(/(https?:\/\/(.+?\.)?discord\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/gm, '');
+const webhookRegex = new RegExp(/(https?:\/\/(.+?\.)?discord(app)?\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/gm, '');
 
 export class WebhookDto  {
     @ApiProperty()
